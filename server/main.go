664785fd0912338f5d0992908d68ee8de7f5c67c
Leonardo Souza <leogsouza@gmail.com>
2018-04-02 17:04:08 +0000

    Add blockchain package

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"net"
 
 	"github.com/leogsouza/grpc-blockchain/proto"
+	"github.com/leogsouza/grpc-blockchain/server/blockchain"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
@@ -17,17 +18,32 @@ func main() {
 	}
 
 	srv := grpc.NewServer()
-	proto.RegisterBlockchainServer(srv, &Server{})
+	proto.RegisterBlockchainServer(srv, &Server{
+		Blockchain: blockchain.NewBlockchain(),
+	})
 	srv.Serve(listener)
 
 }
 
-type Server struct{}
-
-func (s *Server) AddBlock(context.Context, *proto.AddBlockRequest) (*proto.AddBlockResponse, error) {
-	return new(proto.AddBlockResponse), nil
+type Server struct {
+	Blockchain *blockchain.Blockchain
 }
 
-func (s *Server) GetBlockchain(context.Context, *proto.GetBlockchainRequest) (*proto.GetBlockchainResponse, error) {
-	return new(proto.GetBlockchainResponse), nil
+func (s *Server) AddBlock(ctx context.Context, in *proto.AddBlockRequest) (*proto.AddBlockResponse, error) {
+	block := s.Blockchain.AddBlock(in.Data)
+	return &proto.AddBlockResponse{
+		Hash: block.Hash,
+	}, nil
+}
+
+func (s *Server) GetBlockchain(ctx context.Context, in *proto.GetBlockchainRequest) (*proto.GetBlockchainResponse, error) {
+	resp := new(proto.GetBlockchainResponse)
+	for _, b := range s.Blockchain.Blocks {
+		resp.Blocks = append(resp.Blocks, &proto.Block{
+			PrevBlockHash: b.PrevBlockHash,
+			Hash:          b.Hash,
+			Data:          b.Data,
+		})
+	}
+	return resp, nil
 }
